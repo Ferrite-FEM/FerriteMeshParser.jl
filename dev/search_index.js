f2bb59911997d756083cdce247a595eb1af6be31
Documenter.jl <documenter@juliadocs.github.io>
2022-02-07 20:53:00 +0000

    build based on 740fa66

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FerriteMeshParser","category":"page"},{"location":"#FerriteMeshParser","page":"Home","title":"FerriteMeshParser","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FerriteMeshParser.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FerriteMeshParser]","category":"page"}]
+}
