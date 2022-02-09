1f4491a5d45c40342064cb9b29373763ca3b6c18
Documenter.jl <documenter@juliadocs.github.io>
2022-02-09 16:59:56 +0000

    build based on 46652d6

---
+++ dev/examples/compact_tension.jl
@@ -0,0 +1,22 @@
+@warn pwd()
+@warn join(readdir(pwd()), ", ")
+
+using Ferrite, FerriteMeshParser
+
+grid = get_ferrite_grid(joinpath(@__DIR__, "compact_tension.inp"))
+
+println(typeof(grid))
+println(unique(typeof.(getcells(grid))))    # The different cell types in the grid
+
+println([(key, length(set)) for (key, set) in getnodesets(grid)])
+println([(key, length(set)) for (key, set) in getcellsets(grid)])
+
+println([(key, length(set)) for (key, set) in getfacesets(grid)])
+
+faceset = create_faceset(grid, getnodeset(grid,"Hole"));
+
+merge!(getfacesets(grid), Dict("HoleManual" => faceset))
+println([(key, length(set)) for (key, set) in getfacesets(grid)])
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
---
+++ dev/examples/user_element.jl
@@ -0,0 +1,15 @@
+@warn pwd()
+@warn join(readdir(pwd()), ", ")
+
+using Ferrite, FerriteMeshParser
+LinearWedge = Ferrite.Cell{3,6,5}
+
+create_cell(::Type{LinearWedge}, node_numbers, ::FerriteMeshParser.AbaqusMeshFormat) = LinearWedge(ntuple(j->node_numbers[j], length(node_numbers)))
+
+grid = get_ferrite_grid(joinpath(@__DIR__, "wedge_element.inp"); user_elements=Dict("C3D6"=>LinearWedge));
+
+println(typeof(grid))
+println(unique(typeof.(getcells(grid))))    # The different cell types in the grid
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
+
