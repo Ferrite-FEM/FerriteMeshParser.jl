05727c2c3723865cdf18427b8ebb3a85017351fb
Knut Andreas Meyer <knutam@gmail.com>
2022-11-14 09:53:18 +0100

    Make celltype union of celltypes rather than promoted type

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,9 @@ using FerriteMeshParser
 using Ferrite
 using Test
 
+# Function to retrieve test fields
+gettestfile(args...) = joinpath(@__DIR__, "test_files", args...)
+
 # Define equality check between grids
 ==(a::Grid,b::Grid) = all([isequal(getfield.((a,b), (name,))...) for name in fieldnames(typeof(a))])
 
@@ -19,7 +22,7 @@ Ferrite.faces(c::SerendipityQuadrilateral) = ((c.nodes[1],c.nodes[2]), (c.nodes[
                          "3D_UnitVolume_LinearHexahedron", "3D_UnitVolume_LinearTetrahedron",
                          "3D_UnitVolume_QuadraticHexahedron", "3D_UnitVolume_QuadraticTetrahedron")
     for base_name in unit_volume_files
-        filename = joinpath(@__DIR__, "test_files", base_name*".inp")
+        filename = gettestfile(base_name*".inp")
         grid = get_ferrite_grid(filename)
         dh = MixedDofHandler(grid)
         unique_celltypes = unique(typeof.(grid.cells))
@@ -51,8 +54,16 @@ Ferrite.faces(c::SerendipityQuadrilateral) = ((c.nodes[1],c.nodes[2]), (c.nodes[
     end
 end
 
+@testset "celltype" begin
+    _getgridtype(::Grid{dim,C}) where {dim,C} = C
+    grid_concrete = get_ferrite_grid(gettestfile("2D_CPE3.inp"))
+    grid_mixed = get_ferrite_grid(gettestfile("2D_CPE3_CPE4R.inp"))
+    @test isconcretetype(_getgridtype(grid_concrete))
+    @test _getgridtype(grid_mixed) == Union{Triangle,Quadrilateral}
+end
+
 @testset "facesetgeneration" begin
-    filename = joinpath(@__DIR__, "test_files", "compact_tension.inp")
+    filename = gettestfile("compact_tension.inp")
     grid = get_ferrite_grid(filename)
     @test create_faceset(grid, getnodeset(grid, "Hole")) == create_faceset(grid, getnodeset(grid, "Hole"), getcellset(grid, "Hole"))    # Test that including cells doesn't change the created sets
 end
@@ -61,22 +72,22 @@ end
     filename = joinpath(@__DIR__, "runtests.jl")
     @test_throws FerriteMeshParser.UndetectableMeshFormatError get_ferrite_grid(filename) 
 
-    filename = joinpath(@__DIR__, "test_files", "twoinstances.inp")
+    filename = gettestfile("twoinstances.inp")
     @test_throws FerriteMeshParser.InvalidFileContent get_ferrite_grid(filename) 
 
-    filename = joinpath(@__DIR__, "test_files", "unsupported_element.inp")
+    filename = gettestfile("unsupported_element.inp")
     @test_throws FerriteMeshParser.UnsupportedElementType get_ferrite_grid(filename) 
 
 end
 
 @testset "ordering" begin
-    filename0 = joinpath(@__DIR__, "test_files", "2D_UnitArea_Linear.inp")
+    filename0 = gettestfile("2D_UnitArea_Linear.inp")
     grid0 = get_ferrite_grid(filename0)
 
-    filename1 = joinpath(@__DIR__, "test_files", "2D_UnitArea_Linear_flipelementorder.inp")
+    filename1 = gettestfile("2D_UnitArea_Linear_flipelementorder.inp")
     grid1 = get_ferrite_grid(filename1)
 
-    filename2 = joinpath(@__DIR__, "test_files", "2D_UnitArea_Linear_perturbnodeorder.inp")
+    filename2 = gettestfile("2D_UnitArea_Linear_perturbnodeorder.inp")
     grid2 = get_ferrite_grid(filename2)
 
     # comparing grid0==grid<i> doesn't work, so check each field:
@@ -87,32 +98,32 @@ end
 end
 
 @testset "caseinsensitive" begin
-    filename = joinpath(@__DIR__, "test_files", "uppercase_test.inp")
+    filename = gettestfile("uppercase_test.inp")
     grid = get_ferrite_grid(filename)
     @test getncells(grid) == 2
     @test getnnodes(grid) == 6
 end
     
 @testset "sets" begin
-    filename = joinpath(@__DIR__, "test_files", "generated_set.inp")
+    filename = gettestfile("generated_set.inp")
     grid = get_ferrite_grid(filename)
     @test getcellset(grid, "lower") == Set(1:8)
     @test getnodeset(grid, "lower") == Set((1,  2,  3,  4,  7,  8,  9, 10, 11, 12, 13, 14, 20, 21, 22))
 
-    filename = joinpath(@__DIR__, "test_files", "2D_UnitArea_Quadratic.inp")
+    filename = gettestfile("2D_UnitArea_Quadratic.inp")
     grid = get_ferrite_grid(filename)
     @test getcellset(grid, "mysetname") == Set((1,))
 end
 
 @testset "custom_cells" begin
-    filename = joinpath(@__DIR__, "test_files", "unsupported_element.inp")
+    filename = gettestfile("unsupported_element.inp")
     grid = get_ferrite_grid(filename; user_elements=Dict("CPS5R"=>Cell{2,4,4}))
     @test getcellset(grid, "CPS5R") == Set((1,))
     @test isa(getcells(grid)[1], Cell{2,4,4})
 end
 
 @testset "specify_file_type" begin
-    filename = joinpath(@__DIR__, "test_files", "2D_UnitArea_Quadratic.inp")
+    filename = gettestfile("2D_UnitArea_Quadratic.inp")
     filename_cp = joinpath(@__DIR__, "tmp.txt")
     cp(filename, filename_cp; force=true)
     grid_cp = get_ferrite_grid(filename_cp; meshformat=FerriteMeshParser.AbaqusMeshFormat())
