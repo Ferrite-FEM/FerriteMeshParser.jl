d4e72bef98c4201002af8181ac00733c7cfb0ef0
Documenter.jl <documenter@juliadocs.github.io>
2024-05-21 06:42:06 +0000

    build based on aa1c3f2

--- dev/examples/compact_tension.jl
+++ dev/examples/compact_tension.jl
@@ -10,9 +10,6 @@ println([(key, length(set)) for (key, set) in Ferrite.getcellsets(grid)])
 
 println([(key, length(set)) for (key, set) in Ferrite.getfacesets(grid)])
 
-faceset = create_faceset(grid, getnodeset(grid,"Hole"));
-
-merge!(Ferrite.getfacesets(grid), Dict("HoleManual" => faceset))
-println([(key, length(set)) for (key, set) in Ferrite.getfacesets(grid)])
+facetset = create_facetset(grid, getnodeset(grid,"Hole"));
 
 # This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
