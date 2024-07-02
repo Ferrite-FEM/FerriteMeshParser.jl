5c57fe429afccdfa677d99ce1b120feb1124970d
Documenter.jl <documenter@juliadocs.github.io>
2024-07-02 08:17:52 +0000

    build based on 488173f

---
+++ previews/PR45/examples/compact_tension.jl
@@ -0,0 +1,15 @@
+using Ferrite, FerriteMeshParser
+
+grid = get_ferrite_grid("compact_tension.inp")
+
+println(typeof(grid))
+println(unique(typeof.(getcells(grid))))    # The different cell types in the grid
+
+println([(key, length(set)) for (key, set) in Ferrite.getnodesets(grid)])
+println([(key, length(set)) for (key, set) in Ferrite.getcellsets(grid)])
+
+println([(key, length(set)) for (key, set) in Ferrite.getfacesets(grid)])
+
+facetset = create_facetset(grid, getnodeset(grid,"Hole"));
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
